7f9fa0a2b3dcf45bed7761d97dcb7dd94f8beea6
wjs-0702 <wjs-0702@163.com>
2015-02-15 13:05:12 +0800

    change to react style

--- src/main/java/com/wangjs/niovsoio/nio/server/NioServerWithSelector.java
+++ src/main/java/com/wangjs/niovsoio/nio/server/NioServerWithSelector.java
@@ -6,41 +6,87 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * NIO Server main class
  */
 public class NioServerWithSelector {
+	/**
+	 * 服务器监听端口
+	 */
+	public static final Integer LISTENER_PORT = 10000;
 	
 	
 	public void startServer() {
 		
-		Selector selector = null;
+		Selector sel = null;
 		
-		try (ServerSocketChannel server = ServerSocketChannel.open()){
-			server.socket().bind(new InetSocketAddress(10000));
+		try (ServerSocketChannel serverChannel = ServerSocketChannel.open()){
+			serverChannel.configureBlocking(false);
+			serverChannel.socket().bind(new InetSocketAddress(LISTENER_PORT));
 			
-			selector = Selector.open();
+			sel = Selector.open();
+			serverChannel.register(sel, SelectionKey.OP_ACCEPT);
 
 			while(true){
+				int readyers = sel.select();
+				if(readyers>0){
+					Set<SelectionKey> selectedKeys = sel.selectedKeys();
+					Iterator<SelectionKey> iterator = selectedKeys.iterator();
 					
-				SocketChannel socketChannel = server.accept();
-				socketChannel.configureBlocking(false);
-
-				socketChannel.register(selector, SelectionKey.OP_CONNECT);				
-				
-				
+					while(iterator.hasNext()){
+						SelectionKey key = (SelectionKey)iterator.next();
+						
+						//remove current key
+						iterator.remove();
+						
+						if(key.isAcceptable()){
+							doAcceptThing(key);
+							continue;
+						}
+						
+						if(key.isReadable()){
+							doReadThing(key);
+							continue;
+						}
+						
+						if(key.isWritable()){
+							doWriteThing(key);
+							continue;
+						}
+					}
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally{
-			if(selector!=null){
+			if(sel!=null){
 				try {
-					selector.close();
+					sel.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
+	}
+	
+	private void doAcceptThing(SelectionKey key) throws IOException{
+		ServerSocketChannel serverChannel = (ServerSocketChannel)key.channel();
+		SocketChannel socketChannel = serverChannel.accept();
+		socketChannel.configureBlocking(false);
+		
+		Selector sel = key.selector();
+		socketChannel.register(sel, SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+	}
+	
+	private void doReadThing(SelectionKey key){
+		SocketChannel socketChannel = (SocketChannel)key.channel();
+		
+	}
+	
+	private void doWriteThing(SelectionKey key){
+		
 	}
 }
