7c06f561d63813d40fa87b6f075e1201e2dd7600
wjs-0702 <wjs-0702@163.com>
2015-02-27 17:53:33 +0800

    change to echo server

--- src/main/java/com/wangjs/netty/officaltut/DiscardServerHandler.java
+++ src/main/java/com/wangjs/netty/officaltut/DiscardServerHandler.java
@@ -11,12 +11,18 @@ public class DiscardServerHandler extends SimpleChannelHandler{
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e)
 			throws Exception {
-		ChannelBuffer buf = (ChannelBuffer)e.getMessage();
+//		ChannelBuffer buf = (ChannelBuffer)e.getMessage();
+//		
+//		while(buf.readable()){
+//			System.out.println((char)buf.readByte());
+//			System.out.flush();
+//		}
 		
-		while(buf.readable()){
-			System.out.println((char)buf.readByte());
-			System.out.flush();
-		}
+		// change to echo server
+		Object message = e.getMessage();
+		e.getChannel().write(message);
+		System.out.println(message);
+		
 	}
 
 	@Override
