a291a507a8ab18eed2ce092cacbd53d428faddd1
wjs-0702 <wjs-0702@163.com>
2015-02-27 17:47:49 +0800

    加入netty官方教程 Discard Server

---
+++ src/main/java/com/wangjs/netty/officaltut/DiscardServer.java
@@ -0,0 +1,32 @@
+package com.wangjs.netty.officaltut;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+public class DiscardServer {
+	public static void main(String[] args) {
+		ServerBootstrap bootstrap = new ServerBootstrap(
+				new NioServerSocketChannelFactory(
+						Executors.newCachedThreadPool(),
+						Executors.newCachedThreadPool()));
+		
+		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+			
+			@Override
+			public ChannelPipeline getPipeline() throws Exception {
+				return Channels.pipeline(new DiscardServerHandler());
+			}
+		});
+		
+		bootstrap.setOption("child.tcpNoDelay", true);
+		bootstrap.setOption("child.keepAlive", true);
+		
+		bootstrap.bind(new InetSocketAddress(8080));
+	}
+}
